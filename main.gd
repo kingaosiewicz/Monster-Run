81afd077cabe618e095e158a893b6a357a6c35ae
kingaosiewicz <136074959+kingaosiewicz@users.noreply.github.com>
2026-04-09 22:01:02 +0200

    Dodałam do main.gd listę :TODO żeby nie zapomnieć co chciałybyśmy mieć w programie

--- main.gd
+++ main.gd
@@ -9,3 +9,17 @@ func _on_quit_pressed() -> void:
 
 func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://world.tscn")
+
+
+# TODO: Zrobić gałęzie na Githubie
+# TODO: Dodać do mapy przeszkody
+# TODO: Jeśli wejdziemy w potwora i odejmie nam HP to niech ten powtór nie ginie tylko dalej jest, a my zostaniemy odrzuceni troche do tyłu jak w Mario
+# TODO: Zrobić kolejne mapy jak ktoś przejdzie poziom i żeby zapisywało, które poziomy już przeszedłeś. Można potem dodać konta graczy? W kodzie jest stworzona
+# możliwość zapisywania HP i Golda, ale jeśli chcemy zrobić w formie poziomów to trzeba będzie zmienić logikę.
+# TODO: Fajnie jakby obiekty do zbierania tworzyły się przed naszym liskiem, żeby ogólnie sens gry był taki, że idziemy do przodu, a nie do tyłu po itemy
+# TODO: Możemy dodać też czas do wykonania poziomu? Chyba tak było w Mario.
+# TODO: Dodać nowe moby. 
+# TODO: Na trudnejszych poziomach więcej mobów, a mniej nagród i trudniejsze przeszkody.
+# TODO: Wykorzystać animację śmierci i bólu, które już są stworzone w kodzie trzeba tylko je dodać.
+# TODO: Dodać nowe animacje przeciskania się przez szczeliny i logikę.
+# TODO: Po przejściu poziomu - animacja WIN
