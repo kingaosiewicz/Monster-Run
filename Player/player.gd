f07bbcc7a34970e58e1080857042478e8306b651
kingaosiewicz <136074959+kingaosiewicz@users.noreply.github.com>
2026-05-17 12:04:13 +0200

    Squashed commit of the following:
    commit 3c658a1911aeb99d7a07ae5c97a064251ab49b95
    Author: kingaosiewicz <136074959+kingaosiewicz@users.noreply.github.com>
    Date:   Sun May 17 12:03:22 2026 +0200
    
        Create LevelSelect.tscn
    
        zapomniałam się zmienić na moją gałąź
    
    commit 02a8937af05587d937e36a4a6a5c84f32606ca46
    Merge: 99f34f0 abc1ed9
    Author: wsluzalek <sluzalek.weronika@gmail.com>
    Date:   Sat May 16 20:01:52 2026 +0200
    
        Merge pull request #6 from kingaosiewicz/nowy-poziom-3
    
        Nowy poziom 3
    
    commit abc1ed922632830d33a3234da800770213265307
    Author: wsluzalek <sluzalek.weronika@gmail.com>
    Date:   Sat May 16 20:01:24 2026 +0200
    
        :DD
    
        troche poprawek przy podłodze poziomu 1 i 2
    
    commit f72a2b98f73f21966757263d876aa9d646efb411
    Author: wsluzalek <sluzalek.weronika@gmail.com>
    Date:   Sat May 16 19:32:34 2026 +0200
    
        poziom 3 gotowy
    
    commit 16daae89c9d032c870608d89836fec3cd94d8353
    Author: wsluzalek <sluzalek.weronika@gmail.com>
    Date:   Sat May 16 19:07:40 2026 +0200
    
        commit przed dodaniem mechaniki piasku jakby sie cos zepsulo
    
    commit 3fcb1324cbb1b3a222676194a0f85f2c4ae864e7
    Author: wsluzalek <sluzalek.weronika@gmail.com>
    Date:   Sat May 16 18:35:17 2026 +0200
    
        jestem w gazie
    
    commit 99f34f0dabcfe37bf3b2183e3a8e93f0981b6de1
    Merge: 5bf577e bf8f3d6
    Author: wsluzalek <sluzalek.weronika@gmail.com>
    Date:   Sat May 16 18:18:38 2026 +0200
    
        Merge pull request #5 from kingaosiewicz/nowy-poziom-2
    
        Nowy poziom 2
    
    commit bf8f3d628a033d0bd4bcbec72b4a900e7ff40a3b
    Author: wsluzalek <sluzalek.weronika@gmail.com>
    Date:   Sat May 16 18:17:55 2026 +0200
    
        ej dobra wyszlo lepiej niz myslalam
    
    commit 72bd2661768e86c053dea16c780f356f31265349
    Author: wsluzalek <sluzalek.weronika@gmail.com>
    Date:   Sat May 16 17:43:22 2026 +0200
    
        zimowy jeszcze bez lodu
    
        do plazowego mozna dodac wode gdzie nie gdzie w miejsca podlogi co jak w nia wpadniesz to spadasz na dół mapy i giniesz
    
    commit 5bf577e5c59a622ed83fa3850f2bcc57d6580e83
    Merge: 1bb870f f19e41e
    Author: wsluzalek <sluzalek.weronika@gmail.com>
    Date:   Sat May 16 16:42:56 2026 +0200
    
        Merge pull request #4 from kingaosiewicz/nowy-poziom-1
    
        wyglada narazie okej ale jest łatwy
    
    commit f19e41ee8c690f519ebdca9e4c79558aee1fa1f9
    Author: wsluzalek <sluzalek.weronika@gmail.com>
    Date:   Sat May 16 16:41:36 2026 +0200
    
        wyglada narazie okej ale jest łatwy

--- Player/player.gd
+++ Player/player.gd
@@ -1,15 +1,25 @@
 extends CharacterBody2D
 
 const SPEED = 300.0
+const ICE_SPEED = 550.0
+const SAND_SPEED = 100.0 # DODANE: Bardzo wolna prędkość na piasku
 const JUMP_VELOCITY = -400.0
 var knockback_power = 300.0
 
 var is_hurt = false
-var is_dead = false # NOWA ZMIENNA
+var is_dead = false 
 
 @onready var animation = get_node("AnimationPlayer")
 
 func _physics_process(delta: float) -> void:
+	# DODANE: Sprawdzanie wpadnięcia do wody / przepaści
+	# Jeśli lisek spadnie poniżej 800 pikseli na osi Y (możesz zwiększyć tę wartość, 
+	# jeśli Twoja mapa jest głębsza), ginie na miejscu bez odrzutu.
+	if global_position.y > 400 and not is_dead:
+		Game.playerHP = 0
+		die(Vector2.ZERO) # Vector2.ZERO = śmierć w miejscu
+		return
+
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -28,6 +38,24 @@ func _physics_process(delta: float) -> void:
 		velocity.y = JUMP_VELOCITY
 		animation.play("Jump")
 	
+	# --- 1. SPRAWDZANIE NAWIERZCHNI (Lód i Piasek) ---
+	var on_ice = false
+	var on_sand = false
+	
+	if is_on_floor():
+		for i in get_slide_collision_count():
+			var collision = get_slide_collision(i)
+			var collider = collision.get_collider() # Zapisujemy obiekt, z którym się zderzyliśmy
+			
+			# ZABEZPIECZENIE: Upewniamy się, że obiekt istnieje, zanim sprawdzimy jego nazwę!
+			if collider != null:
+				var collider_name = collider.name
+				
+				if collider_name == "lod":
+					on_ice = true
+				elif collider_name == "piasek":
+					on_sand = true
+
 	var direction := Input.get_axis("ui_left", "ui_right")
 	
 	if direction == -1:
@@ -35,14 +63,37 @@ func _physics_process(delta: float) -> void:
 	elif direction == 1:
 		get_node("AnimatedSprite2D").flip_h = false
 		
-	if direction:
-		velocity.x = direction * SPEED
-		if velocity.y == 0:
-			animation.play("Run")
+	# --- 2. RUCH LISKA (zależnie od nawierzchni) ---
+	if on_ice:
+		# LIS JEST NA LODZIE
+		if direction:
+			velocity.x = move_toward(velocity.x, direction * ICE_SPEED, 10.0) 
+			if velocity.y == 0:
+				animation.play("Run")
+		else:
+			velocity.x = move_toward(velocity.x, 0, 0.5) 
+			if velocity.y == 0:
+				animation.play("Idle")
+	elif on_sand:
+		# LIS JEST NA PIASKU
+		if direction:
+			velocity.x = direction * SAND_SPEED # Ograniczona, powolna prędkość
+			if velocity.y == 0:
+				animation.play("Run")
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED) # Zwykłe zatrzymanie
+			if velocity.y == 0:
+				animation.play("Idle")
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		if velocity.y == 0:
-			animation.play("Idle")
+		# LIS JEST NA ZIEMI (Standardowy ruch)
+		if direction:
+			velocity.x = direction * SPEED
+			if velocity.y == 0:
+				animation.play("Run")
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED) 
+			if velocity.y == 0:
+				animation.play("Idle")
 	
 	if velocity.y > 0:
 		animation.play("Fall")
@@ -51,26 +102,23 @@ func _physics_process(delta: float) -> void:
 
 # ZMIENIONA FUNKCJA OTRZYMYWANIA OBRAŻEŃ
 func take_damage(knockback_dir: Vector2):
-	if is_dead: return # Nie dostajemy obrażeń, jeśli już nie żyjemy
+	if is_dead: return 
 	
-	# 1. SPRAWDZAMY CZY TO ŚMIERTELNY CIOS (Zanim zrobimy cokolwiek innego)
 	if Game.playerHP <= 0:
-		die(knockback_dir) # Od razu wywołujemy śmierć z kierunkiem uderzenia
-		return # Przerywamy czytanie reszty tej funkcji!
+		die(knockback_dir) 
+		return 
 	
-	# 2. ZWYKŁE OBRYWANIE (Jeśli lisek ma jeszcze HP)
 	is_hurt = true
 	animation.play("Hurt")
 	
 	velocity.y = -250.0 
 	velocity.x = knockback_dir.x * knockback_power
 	
-	# Czekamy na koniec animacji bólu
 	await animation.animation_finished
 	is_hurt = false
 
 
-# ZMIENIONA FUNKCJA ŚMIERCI (Teraz przyjmuje wektor uderzenia)
+# ZMIENIONA FUNKCJA ŚMIERCI
 func die(knockback_dir: Vector2):
 	is_dead = true
 	
@@ -79,9 +127,14 @@ func die(knockback_dir: Vector2):
 	
 	get_node("CollisionShape2D").set_deferred("disabled", true)
 	
-	# Śmiertelny odrzut od żaby - lisek leci wyżej (-400) i w tył!
-	velocity.y = -400.0 
-	velocity.x = knockback_dir.x * knockback_power
+	# Jeśli wektor uderzenia to ZERO (czyli woda), lisek po prostu opada w dół
+	if knockback_dir == Vector2.ZERO:
+		velocity.y = 100.0
+		velocity.x = 0
+	else:
+		# Śmiertelny odrzut od żaby
+		velocity.y = -400.0 
+		velocity.x = knockback_dir.x * knockback_power
 	
 	await get_tree().create_timer(2.0).timeout
 	
