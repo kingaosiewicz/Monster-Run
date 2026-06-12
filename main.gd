a9f453b7d1b0bc71e04c346c81dfce3597fbf432
kingaosiewicz <136074959+kingaosiewicz@users.noreply.github.com>
2026-06-12 12:56:09 +0200

    Aktualizacja listy TODO

--- main.gd
+++ main.gd
@@ -10,13 +10,11 @@ func _on_quit_pressed() -> void:
 func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://level-scenes/LevelSelect.tscn")
 
-# TODO: Dodać do mapy przeszkody
 # TODO: Zrobić kolejne mapy jak ktoś przejdzie poziom i żeby zapisywało, które poziomy już przeszedłeś. Można potem dodać konta graczy? W kodzie jest stworzona
 # możliwość zapisywania HP i Golda, ale jeśli chcemy zrobić w formie poziomów to trzeba będzie zmienić logikę.
 # TODO: Fajnie jakby obiekty do zbierania tworzyły się przed naszym liskiem, żeby ogólnie sens gry był taki, że idziemy do przodu, a nie do tyłu po itemy
 # TODO: Możemy dodać też czas do wykonania poziomu? Chyba tak było w Mario.
 # TODO: Dodać nowe moby. 
 # TODO: Na trudnejszych poziomach więcej mobów, a mniej nagród i trudniejsze przeszkody.
-# TODO: Wykorzystać animację śmierci i bólu, które już są stworzone w kodzie trzeba tylko je dodać.
 # TODO: Dodać nowe animacje przeciskania się przez szczeliny i logikę.
 # TODO: Po przejściu poziomu - animacja WIN
