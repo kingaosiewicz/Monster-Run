17707374dba5a43a09055ef23092a7939ad43dab
kingaosiewicz <136074959+kingaosiewicz@users.noreply.github.com>
2026-06-26 12:48:52 +0200

    Szczur atak

--- szczur.gd
+++ szczur.gd
@@ -44,6 +44,29 @@ func _on_player_detection_body_exited(body: Node2D) -> void:
 
 
 func _on_player_death_body_entered(body: Node2D) -> void:
-	#if body.name == "Player":
-		#death()
-		pass
+	if body.name == "Player":
+		death()
+
+func _on_player_collision_body_entered(body: Node2D) -> void:
+		if body.name == "Player":
+			if Game.is_invincible:
+				return
+		Game.playerHP -= 3
+
+		
+		# Obliczamy wektor kierunku - z której strony żaba uderzyła gracza
+		var knockback_dir = (body.global_position - self.global_position).normalized()
+		
+		# Zamiast death(), wywołujemy nową funkcję w graczu:
+		body.take_damage(knockback_dir)
+
+func death():
+	Game.Gold += 3
+	Utils.saveGame()
+	chase = false
+	velocity.x = 0 
+	get_node("CollisionShape2D").set_deferred("disabled", true)
+	get_node("AnimatedSprite2D").play("Death")
+	$DzwiekZgniatania.play()
+	await get_node("AnimatedSprite2D").animation_finished
+	self.queue_free()
